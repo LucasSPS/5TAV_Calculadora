978efd96a0fffc0704c659a1f2492de2a25ddf87
unknown <you@example.comlucass.oficial@gmail.com>
2021-09-18 13:59:34 -0300

    adding files

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+package calculadora;
+
+public class Calculadora {
+	public float somar(float a, float b) {
+		return a + b;
+	}
+
+	public float subtrair(float a, float b) {
+		return a - b;
+	}
+
+	public float multiplicar(float a, float b) {
+		return a * b;
+	}
+
+	public float dividir(float a, float b) {
+		return a / b;
+	}
+
+}
